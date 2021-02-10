aadd9b0363e25863072108b4e21440c6d2f835f3
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-02-10 17:28:12 +0100

    extra checks and public methods

--- third/modal.js
+++ third/modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
+		this.isOpen = false;
 		this.shadowRoot.innerHTML = `
 			<style>
 				#backdrop {
@@ -77,23 +78,27 @@ class Modal extends HTMLElement {
 		`;
 	}
 	
-//	attributeChangedCallback(name, oldValue, newValue) {
-//		if (name === 'opened') {
-//			if (this.hasAttribute('opened')) {
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === 'opened') {
+			if (this.hasAttribute('opened')) {
+				this.isOpen = true;
 //				this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
 //				this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
 //				this.shadowRoot.querySelector('#modal').style.opacity = 1;
 //				this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-//			}
-//		}
-//	}
-//	
-//	static get observedAttributes() {
-//		return ['opened'];
-//	}
+			}
+		} else {
+			this.isOpen = false;
+		}
+	}
+	
+	static get observedAttributes() {
+		return ['opened'];
+	}
 
 	open() {
 		this.setAttribute('opened', '')
+		this.isOpen = true;
 	}
 }
 
